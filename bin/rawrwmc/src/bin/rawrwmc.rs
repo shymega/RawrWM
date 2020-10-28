5ff303838c85e9068f24a136691b90d363eb5bc4
Dom Rodriguez <shymega@shymega.org.uk>
2020-10-28 17:59:57 +0000

    Run rustfmt on source
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- bin/rawrwmc/src/bin/rawrwmc.rs
+++ bin/rawrwmc/src/bin/rawrwmc.rs
@@ -1,3 +1,20 @@
+extern crate clap;
+
+use clap::{
+    App,
+    ArgMatches,
+};
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+fn parse_arguments() -> ArgMatches<'static> {
+    App::new("rawrwmc")
+        .author("The RawrWM Developers")
+        .version(VERSION)
+        .get_matches()
+}
+
 fn main() {
+    let _matches = parse_arguments();
     unimplemented!();
 }
