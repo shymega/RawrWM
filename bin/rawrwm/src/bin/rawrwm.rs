d9a64f845b8e53eabf91e5c63c5e701b5180a4bf
Dom Rodriguez <shymega@shymega.org.uk>
2020-11-21 23:19:59 +0000

    Add argument parsing to bin/rawrwm binary
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- bin/rawrwm/src/bin/rawrwm.rs
+++ bin/rawrwm/src/bin/rawrwm.rs
@@ -1,3 +1,17 @@
+extern crate clap;
+
+use clap::{App, ArgMatches};
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+fn parse_arguments() -> ArgMatches<'static> {
+    App::new("rawrwm")
+        .author("The RawrWM Developers")
+        .version(VERSION)
+        .get_matches()
+}
+
 fn main() {
-    unimplemented!()
+    let _matches = parse_arguments();
+    unimplemented!();
 }
