3214b91836c7928db43e037054521d15a454dea1
Dom Rodriguez <shymega@shymega.org.uk>
2022-12-24 23:44:36 +0000

    Add better error handling
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- bin/rawrd/src/main.rs
+++ bin/rawrd/src/main.rs
@@ -16,33 +16,42 @@
 
 use std::io::Error;
 
+use anyhow::{Context, Result};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum AppStartError {
+    #[error("Initialization error")]
+    InitializationError,
+}
+
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[cfg(target_os = "windows")]
-async fn windows_main() -> Result<(), Error> {
+async fn windows_main() -> Result<(), AppStartError> {
     Ok(())
 }
 
 #[cfg(target_os = "macos")]
-async fn macos_main() -> Result<(), Error> {
+async fn macos_main() -> Result<(), AppStartError> {
     Ok(())
 }
 
 #[cfg(target_os = "linux")]
-async fn linux_main() -> Result<(), Error> {
+async fn linux_main() -> Result<(), AppStartError> {
     Ok(())
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<()> {
     #[cfg(target_os = "windows")]
-    windows_main().await?;
+    windows_main().await.context("Initialization error?")?;
 
     #[cfg(target_os = "macos")]
-    macos_main().await?;
+    macos_main().await.context("Initialization error?")?;
 
     #[cfg(target_os = "linux")]
-    linux_main().await?;
+    linux_main().await.context("Initialization error?")?;
 
     Ok(())
 }
